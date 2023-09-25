942e6846c064c6c3abc7d2932f065631aa099378
Igor Konchakovskii <northernfenian@gmail.com>
2023-09-26 00:29:26 +0300

    for cycle

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const numberOfFilms = +prompt("How many films have you watched?");
 
 
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,12 +11,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("What is your latest movie watched?");
-    b = prompt("How do you rate it?");
-    c = prompt("What is your latest movie watched?");
-    d = prompt("How do you rate it?");
+if (personalMovieDB.count < 10) {
+    alert("Very few films");
+} else if (personalMovieDB.count < 30) {
+    alert("Classic");
+} else if (personalMovieDB.count > 30) {
+    alert("Fan!");
+} else {
+    alert("error!");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const x = prompt("What is your latest movie watched?");
+    const y = prompt("How do you rate it?");
+    if (x != null && y != null && x != '' && y != '' && x.length < 50) {
+        personalMovieDB.movies[x] = y;
+        console.log('Done');
+    } else {
+        console.log ("error");
+        i--;
+    }
+    
+}
+
+
 
 console.log(personalMovieDB);
