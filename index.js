07efb3101e1afa82bb75cb241f9c3b5816ad5fa4
Igor Konchakovskii <northernfenian@gmail.com>
2023-09-26 12:01:42 +0300

    for cycle

--- index.js
+++ index.js
@@ -11,42 +11,42 @@ const personalMovieDB = {
     privat: false
 };
 
-// if (personalMovieDB.count < 10) {
-//     alert("Very few films");
-// } else if (personalMovieDB.count < 30) {
-//     alert("Classic");
-// } else if (personalMovieDB.count > 30) {
-//     alert("Fan!");
-// } else {
-//     alert("error!");
-// }
+if (personalMovieDB.count < 10) {
+    alert("Very few films");
+} else if (personalMovieDB.count < 30) {
+    alert("Classic");
+} else if (personalMovieDB.count > 30) {
+    alert("Fan!");
+} else {
+    alert("error!");
+}
 
-// for (let i = 0; i < 2; i++) {
-//     const x = prompt("What is your latest movie watched?");
-//     const y = prompt("How do you rate it?");
-//     if (x != null && y != null && x != '' && y != '' && x.length < 50) {
-//         personalMovieDB.movies[x] = y;
-//         console.log('Done');
-//     } else {
-//         console.log ("error");
-//         i--;
-//     }
-    
-// }
-
-let i = 0;
-do {
+for (let i = 0; i < 2; i++) {
     const x = prompt("What is your latest movie watched?");
     const y = prompt("How do you rate it?");
     if (x != null && y != null && x != '' && y != '' && x.length < 50) {
         personalMovieDB.movies[x] = y;
         console.log('Done');
-        i++;
     } else {
         console.log ("error");
-        i = i;
+        i--;
     }
-} while (i < 2);
+    
+}
+
+// let i = 0;
+// do {
+//     const x = prompt("What is your latest movie watched?");
+//     const y = prompt("How do you rate it?");
+//     if (x != null && y != null && x != '' && y != '' && x.length < 50) {
+//         personalMovieDB.movies[x] = y;
+//         console.log('Done');
+//         i++;
+//     } else {
+//         console.log ("error");
+//         i = i;
+//     }
+// } while (i < 2);
 
 
 console.log(personalMovieDB);
